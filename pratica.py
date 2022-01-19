cc105c0c33012b89a4e44f3dcb6d42d24ce2099a
PedroMarzano <marzanopedro5@gmail.com>
2022-01-19 10:06:58 -0300

    Opa, mandando o teste que eu fiz da última vez, Só coloquei na "prática" pra gente testar

--- pratica.py
+++ pratica.py
@@ -1,11 +1,36 @@
 import pygame
 from sys import exit
 
-# global variables / variaveis globais
-WINDOW_WIDTH = 800
-WINDOW_LENGTH = 800
-SCREEN = None
+def drawitself(a,b,alive): #(coordinates/alive state) :
+    #only draws if cell is alive
+    if alive:
+        blockSize = 15
+        Rect = pygame.Rect(a, b, blockSize, blockSize)
+        pygame.draw.rect(SCREEN, GREY, Rect, width=blockSize)
+    else:
+        pass
 
+class cell:
+  def __init__(self,a,b):
+    self.alive = True
+    self.neighbors = set()
+    self.coordinates = [a,b] #cell coordinates
+    self.appear = drawitself(a,b,self.alive) 
+
+  def isAlive(self):
+    return self.alive
+  
+  def addNeighbor(self, newNeighbor):
+    self.neighbors.add(newNeighbor)
+  
+  def getNeighbors(self):
+    return self.neighbors
+
+# global variables / variaveis globais
+WINDOW_WIDTH = 720
+WINDOW_LENGTH = 720
+SCREEN = None
+blockSize = 15
 #global colors
 BLACK = (0, 0, 0)
 GREY = (150, 150, 150)
@@ -14,12 +39,11 @@ GREY = (150, 150, 150)
   drawGrid will be responsible for drawing the grid for the game of life
 """
 def drawGrid():
-  global WINDOW_WIDTH, WINDOW_LENGTH, WHITE
+  global WINDOW_WIDTH, WINDOW_LENGTH, WHITE, blockSize
   
   #size of squares
-  blockSize = 15
-
   
+
   for x in range(0, WINDOW_WIDTH, blockSize):
     for y in range(0, WINDOW_LENGTH, blockSize):
       rect = pygame.Rect(x, y, blockSize, blockSize)
@@ -41,6 +65,9 @@ def main():
   # infinite loop to keep game running
   while (True):
     drawGrid()
+    cell(0,0) #create the cells
+
+
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
@@ -48,6 +75,14 @@ def main():
     
     pygame.display.update()
 
+#def GenerateCoordinates():
+    #global WINDOW_WIDTH, WINDOW_LENGTH, blockSize
+
+    #numberOfColumns = WINDOW_WIDTH / blockSize
+    #numberOfLines = WINDOW_LENGTH / blockSize
+    
+
 
 if __name__ == "__main__":
   main()
+
